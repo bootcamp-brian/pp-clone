d887fa18d7b6d28bfee0c4d0f86e2c4ee8ece4ac
brian <braintm2@gmail.com>
2023-08-02 15:30:22 -0700

    fixed up landing page and cleaned up some code

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -111,127 +111,129 @@ const Home = () => {
         renderStoreFront();
     }, [puppiesStartIndex, puppiesEndIndex, selectedCategories])
 
-    return (<>
-        {
-            isLoading && <Loading />
-        }
-        <ThemeProvider theme={theme}>
-            <Container component="main" maxWidth="xl">
-                {
-                    Object.keys(featuredPuppy).length === 0 &&
-                    <Autocomplete
-                        multiple
-                        id="category-select"
-                        options={allCategories}
-                        getOptionLabel={(option) => option.name}
-                        value={selectedCategories}
-                        onChange={handleCategorySelect}
-                        isOptionEqualToValue={(option, value) => option.id === value.id}
-                        sx={{ mb: 2, background: "white" }}
-                        renderInput={(params) => (
-                        <TextField
-                            {...params}
-                            label="Sorting Categories"
-                            placeholder="Categories"
-                        />
-                        )}
-                    />
-                }
-                <Grid container spacing={2} sx={{ display: 'flex', alignItems: 'end' }}>
+    return (
+        <>
+            {
+                isLoading && <Loading />
+            }
+            <ThemeProvider theme={theme}>
+                <Container component="main" maxWidth="xl">
                     {
-                        Object.keys(featuredPuppy).length === 0
-                        ?
-                        puppies.map(puppy => {
-                            const { id, image1, name, age, size, price, breed } = puppy;
-                            let inCart = false;
-                            for (let item of cartItems) {
-                                if ((token ? item.puppyId : item.id) === puppy.id) {
-                                    inCart = true;
-                                    break;
+                        Object.keys(featuredPuppy).length === 0 &&
+                        <Autocomplete
+                            multiple
+                            id="category-select"
+                            options={allCategories}
+                            getOptionLabel={(option) => option.name}
+                            value={selectedCategories}
+                            onChange={handleCategorySelect}
+                            isOptionEqualToValue={(option, value) => option.id === value.id}
+                            sx={{ mb: 2, background: "white" }}
+                            renderInput={(params) => (
+                            <TextField
+                                {...params}
+                                label="Sorting Categories"
+                                placeholder="Categories"
+                            />
+                            )}
+                        />
+                    }
+                    <Grid container spacing={2} sx={{ display: 'flex', alignItems: 'end' }}>
+                        {
+                            Object.keys(featuredPuppy).length === 0
+                            ?
+                            puppies.map(puppy => {
+                                const { id, image1, name, age, size, price, breed } = puppy;
+                                let inCart = false;
+                                for (let item of cartItems) {
+                                    if ((token ? item.puppyId : item.id) === puppy.id) {
+                                        inCart = true;
+                                        break;
+                                    }
                                 }
-                            }
-                            
-                            return <PuppyCard
-                                key={id}
-                                id={id}
-                                imgURL={image1}
-                                name={name}
-                                age={age}
-                                size={size}
-                                price={price}
-                                breed={breed}
+                                
+                                return <PuppyCard
+                                    key={id}
+                                    id={id}
+                                    imgURL={image1}
+                                    name={name}
+                                    age={age}
+                                    size={size}
+                                    price={price}
+                                    breed={breed}
+                                    setFeaturedPuppy={setFeaturedPuppy}
+                                    setIsLoading={setIsLoading}
+                                    token={token}
+                                    cartItems={cartItems}
+                                    setCartItems={setCartItems}
+                                    inCart={inCart}
+                                />
+                            })
+                            :
+                            <FeaturedDetails
+                                featuredPuppy={featuredPuppy}
                                 setFeaturedPuppy={setFeaturedPuppy}
                                 setIsLoading={setIsLoading}
                                 token={token}
                                 cartItems={cartItems}
                                 setCartItems={setCartItems}
-                                inCart={inCart}
                             />
-                        })
-                        :
-                        <FeaturedDetails
-                            featuredPuppy={featuredPuppy}
-                            setFeaturedPuppy={setFeaturedPuppy}
-                            setIsLoading={setIsLoading}
-                            token={token}
-                            cartItems={cartItems}
-                            setCartItems={setCartItems}
-                        />
+                        }
+                    </Grid>
+                    <Box
+                        sx={{
+                            display: 'flex',
+                            mt: 2,
+                            justifyContent: 
+                                puppiesStartIndex === 0 ? 'end' :
+                                    (puppiesEndIndex >= puppiesTotal ? 'start'
+                                    :
+                                    'space-between')
+                        }}
+                    >
+                    {
+                            puppiesStartIndex !== 0 && Object.keys(featuredPuppy).length === 0 &&
+                                <Button
+                                    variant="contained"
+                                    size="medium"
+                                    color="primary"
+                                    onClick={handlePrevButtonClick}
+                                    sx={{
+                                        mt: 1,
+                                        mb: 1,
+                                        background: '#768087',
+                                        ":hover": {
+                                        bgcolor: "#5e666c",
+                                        color: "white" }
+                                    }}
+                                >
+                                    {'<<'} Prev
+                                </Button>
                     }
-                </Grid>
-                <Box
-                    sx={{
-                        display: 'flex',
-                        mt: 2,
-                        justifyContent: 
-                            puppiesStartIndex === 0 ? 'end' :
-                                (puppiesEndIndex >= puppiesTotal ? 'start'
-                                :
-                                'space-between')
-                    }}
-                >
-                {
-                        puppiesStartIndex !== 0 && Object.keys(featuredPuppy).length === 0 &&
-                            <Button
-                                variant="contained"
-                                size="medium"
-                                color="primary"
-                                onClick={handlePrevButtonClick}
-                                sx={{
-                                    mt: 1,
-                                    mb: 1,
-                                    background: '#768087',
-                                    ":hover": {
-                                    bgcolor: "#5e666c",
-                                    color: "white" }
-                                }}
-                            >
-                                {'<<'} Prev
-                            </Button>
-                }
-                {
-                        puppiesTotal - puppiesEndIndex > 0 && Object.keys(featuredPuppy).length === 0 &&
-                            <Button
-                                variant="contained"
-                                size="medium"
-                                color="primary"
-                                onClick={handleNextButtonClick}
-                                sx={{
-                                    mt: 1,
-                                    mb: 1,
-                                    background: '#768087',
-                                    ":hover": {
-                                    bgcolor: "#5e666c",
-                                    color: "white" }
-                                }}
-                            >
-                                Next {'>>'}
-                            </Button>
-                }
-                </Box>
-            </Container>
-        </ThemeProvider>
-    </>)
+                    {
+                            puppiesTotal - puppiesEndIndex > 0 && Object.keys(featuredPuppy).length === 0 &&
+                                <Button
+                                    variant="contained"
+                                    size="medium"
+                                    color="primary"
+                                    onClick={handleNextButtonClick}
+                                    sx={{
+                                        mt: 1,
+                                        mb: 1,
+                                        background: '#768087',
+                                        ":hover": {
+                                        bgcolor: "#5e666c",
+                                        color: "white" }
+                                    }}
+                                >
+                                    Next {'>>'}
+                                </Button>
+                    }
+                    </Box>
+                </Container>
+            </ThemeProvider>
+        </>
+    )
 }
 
 export default Home;
--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,41 +1,57 @@
-import { Button } from "@mui/material";
+import { Button, Container, LinearProgress, Typography } from "@mui/material";
 import { getAvailablePuppies } from "../utils/API";
 import { useNavigate } from "react-router-dom";
 import landingImage from "./images/landing-image.png"
+import { useEffect, useState } from "react";
 
 const Landing = () => {
     const navigate = useNavigate();
-    // const spinUp = async () => {
-    //     getAvailablePuppies();
-    // }
+    const [loading, setLoading] = useState(true);
     const handleClick = () => {
         navigate("/home");
     }
-    getAvailablePuppies();
+
+    useEffect(() => {
+        const retrievePuppies = async () => {
+            await getAvailablePuppies();
+            setLoading(false);
+        }
+        retrievePuppies();
+    }, []);
 
     return (
         <>
             <div className="landing">
                 <img className="landingImage" src={landingImage} alt="" />
-                <Button
-                    variant="contained"
-                    size="medium"
-                    onClick={handleClick}
-                    sx={{
-                        mr: 1,
-                        background: 'green',
-                        ":hover": {
-                            bgcolor: "#106B21",
-                            color: "white" },
-                        "&.Mui-disabled": {
-                            background: "gray",
-                            color: "white",
-                            opacity: 0.8
-                        }
-                    }}
-                >
-                    Browse Puppies
-                </Button>
+                {
+                    loading ?
+                        <Container sx={{ }}>
+                            <Typography variant="h4" component="h2" align="center" sx={{ color: "#eff0eb" }}>
+                                Retrieving Puppies...
+                            </Typography>
+                            <LinearProgress color="inherit" sx={{ width: "50%", ml: "25%", mt: 1 }}/>
+                        </Container>
+                    :
+                    <Button
+                        variant="contained"
+                        size="large"
+                        onClick={handleClick}
+                        sx={{
+                            mr: 1,
+                            background: 'green',
+                            ":hover": {
+                                bgcolor: "#106B21",
+                                color: "white" },
+                            "&.Mui-disabled": {
+                                background: "gray",
+                                color: "white",
+                                opacity: 0.8
+                            }
+                        }}
+                    >
+                        Browse Puppies
+                    </Button>
+                }
             </div>
         </>
     )
